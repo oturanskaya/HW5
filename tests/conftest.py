7b9814e9c24fdae7a0c55ed0fb9a7bdddcbdce3e
oturanskaya <turolevna@gmail.com>
2023-08-11 19:50:45 +0300

    work_on_mistakes

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,11 @@ from selene import browser
 @pytest.fixture(scope="function", autouse=True)
 def setup_browser():
 
-    browser.config.driver_name = 'firefox'
+    #browser.config.driver_name = 'firefox'
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_height = 768
     browser.config.window_width = 1024
-    browser.config.timeout = 2000.0
+    browser.config.timeout = 6.0
 
     yield
 
--- tests/test_HW_5.py
+++ tests/test_HW_5.py
@@ -1,10 +1,14 @@
 import os
 
-from selene import browser
+from selene import browser, command
 from selene.support.conditions import be, have
 
 def test_registration_form(setup_browser):
     browser.open('/automation-practice-form')
+    browser.execute_script('document.querySelector("#fixedban").remove()')
+    browser.element('footer').execute_script('element.remove()')
+
+
     browser.element('#firstName').type('Olga')
     browser.element('#lastName').type('Turanskaya')
     browser.element('#userEmail').type('tur@mail.ru')
@@ -15,6 +19,11 @@ def test_registration_form(setup_browser):
     browser.element('.react-datepicker__year-select>option[value="1999"]').click()
     browser.element('.react-datepicker__day--014').click()
     browser.element('#subjectsInput').type('English').press_enter()
+
+    #browser.element('[#subjectsInput"]').perform(command.js.scroll_into_view)
+
+    browser.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
     browser.element('[for="hobbies-checkbox-1"]').click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('resources/fem_red.png'))
     browser.element('#currentAddress').type('Moscow, Red Square, 1')
@@ -22,9 +31,15 @@ def test_registration_form(setup_browser):
     browser.element('#react-select-4-input').type('Delhi').press_enter()
     browser.element('#submit').click()
 
-    browser.element('.table-responsive').should(have.text('Olga' and 'Turanskaya' and 'tur@mail.ru' and '+7123456789'
-                                                          and '14 January,1999' and 'English' and 'Sports' and 'Moscow, Red Square, 1'
-                                                          and 'NCR Delhi'))
+    browser.element('.table').should(have.text('Olga Turanskaya'))
+    browser.element('.table').should(have.text('tur@mail.ru'))
+    browser.element('.table').should(have.text('14 February,1999'))
+    browser.element('.table').should(have.text('Female'))
+    browser.element('.table').should(have.text('7123456789'))
+    browser.element('.table').should(have.text('English'))
+    browser.element('.table').should(have.text('Sports'))
+    browser.element('.table').should(have.text('Moscow, Red Square, 1'))
+    browser.element('.table').should(have.text('NCR Delhi'))
 
     browser.element('#closeLargeModal').click()
 
